4753f7c74131a1df9eba7bbdc89f114c46a993af
ytgs5148 <naturephotos5148@gmail.com>
2024-09-03 21:23:09 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main style={{ backgroundColor: '#282c34', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+        <main style={{ backgroundColor: '#d6dcdc', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
             <i>Page content</i>
         </main>
     );
